c5244c2d9075b09ea6975694e2e08bb9dfc60c97
jecihjoy <jecihjoy@gmail.com>
2018-11-14 16:47:41 +0300

    added travis files route testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,89 +1,89 @@
-// Karma configuration
-// Generated on Tue Nov 13 2018 10:40:23 GMT+0300 (E. Africa Standard Time)
+// // Karma configuration
+// // Generated on Tue Nov 13 2018 10:40:23 GMT+0300 (E. Africa Standard Time)
 
-module.exports = function (config) {
-  config.set({
+// module.exports = function (config) {
+//   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-    plugins: [
-      'karma-jasmine',
-      'karma-webpack',
-      'karma-coverage',
-      'karma-remap-istanbul',
-      'karma-chrome-launcher',
-    ],
-
-    // list of files / patterns to load in the browser
-    files: [
-    ],
+//     // base path that will be used to resolve all patterns (eg. files, exclude)
+//     basePath: '',
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+//     // frameworks to use
+//     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+//     frameworks: ['jasmine'],
+
+//     plugins: [
+//       'karma-jasmine',
+//       'karma-webpack',
+//       'karma-coverage',
+//       'karma-remap-istanbul',
+//       'karma-chrome-launcher',
+//     ],
+
+//     // list of files / patterns to load in the browser
+//     files: [
+//     ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      '**/src/**/*.spec.(ts|js)':
-       ['sourcemap', 'webpack', 'coverage']
-    },
+//     // list of files to exclude
+//     exclude: [
+//     ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    // reporters: ['progress', 'coverage', 'remap-coverage'],
-    reporters: [ 'coverage'],
+//     // preprocess matching files before serving them to the browser
+//     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+//     preprocessors: {
+//       '**/src/**/*.spec.(ts|js)':
+//        ['sourcemap', 'webpack', 'coverage']
+//     },
+
+
+//     // test results reporter to use
+//     // possible values: 'dots', 'progress'
+//     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+//     // reporters: ['progress', 'coverage', 'remap-coverage'],
+//     reporters: [ 'coverage'],
 
     
-    coverageReporter = {
-      type : 'html',
-      dir : 'coverage/'
-    },
+//     coverageReporter = {
+//       type : 'html',
+//       dir : 'coverage/'
+//     },
 
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+//     // web server port
+//     port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+//     // enable / disable colors in the output (reporters and logs)
+//     colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+//     // level of logging
+//     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+//     logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browbrowsers: ['Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+//     // enable / disable watching file and executing tests whenever any file changes
+//     autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+//     // start these browsers
+//     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+//     browbrowsers: ['Chrome'],
+//     customLaunchers: {
+//       ChromeHeadlessCI: {
+//         base: 'ChromeHeadless',
+//         flags: ['--no-sandbox']
+//       }
+//     },
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+
+//     // Continuous Integration mode
+//     // if true, Karma captures browsers, runs the tests and exits
+//     singleRun: false,
+
+//     // Concurrency level
+//     // how many browser should be started simultaneous
+//     concurrency: Infinity
+//   })
+// }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,38 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
 describe('AppComponent', () => {
   let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule.withRoutes([]) ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
-
-    component = TestBed.get(AppComponent);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   }));
+
+  it('should be created', () => {
+    expect(component).toBeDefined();
+  })
+
+  it('should have the router outlet', () => {
+   let variable =  fixture.debugElement.query(By.directive(RouterOutlet));
+   expect(variable).not.toBeNull();
+  })
+
+  it('should have a route that points to weatherMap page', () => {
+    let elements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    let index = elements.findIndex(i => i.properties['href'] === '/weather-map');
+    expect(index).toBe(0);
+    // expect(index).toBeGreaterThan(0);
+  })
 });
